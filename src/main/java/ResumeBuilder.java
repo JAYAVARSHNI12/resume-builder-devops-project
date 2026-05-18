055757c876db182b758564e7640f298e13f969f9
JAYAVARSHNI S <jayavarshnisanthoshkumar@gmail.com>
2026-05-18 14:03:24 +0530

    Add generateResume method for enhanced resume creation

--- src/main/java/ResumeBuilder.java
+++ src/main/java/ResumeBuilder.java
@@ -3,37 +3,57 @@ import java.io.IOException;
 
 public class ResumeBuilder {
 
-    public static void saveResume(
+    public static void generateResume(
+
             String name,
+            String role,
             String email,
             String phone,
             String address,
+            String summary,
             String skills,
             String education,
+            String experience,
             String projects,
-            String summary) {
+            String languages
+
+    ) {
 
         String resume =
+
                 "============================\n" +
-                "         RESUME\n" +
+                "      SMART RESUME\n" +
                 "============================\n\n" +
 
                 "Name: " + name + "\n" +
+                "Role: " + role + "\n" +
                 "Email: " + email + "\n" +
                 "Phone: " + phone + "\n" +
                 "Address: " + address + "\n\n" +
 
                 "SUMMARY\n" +
+                "----------------------------\n" +
                 summary + "\n\n" +
 
                 "SKILLS\n" +
+                "----------------------------\n" +
                 skills + "\n\n" +
 
                 "EDUCATION\n" +
+                "----------------------------\n" +
                 education + "\n\n" +
 
+                "EXPERIENCE\n" +
+                "----------------------------\n" +
+                experience + "\n\n" +
+
                 "PROJECTS\n" +
-                projects + "\n";
+                "----------------------------\n" +
+                projects + "\n\n" +
+
+                "LANGUAGES\n" +
+                "----------------------------\n" +
+                languages;
 
         try {
 
@@ -43,18 +63,19 @@ public class ResumeBuilder {
 
             writer.close();
 
-            System.out.println("Resume saved successfully.");
+            System.out.println("Resume generated successfully");
 
         }
 
         catch (IOException e) {
 
-            System.out.println("Error saving resume.");
+            System.out.println("Error generating resume");
         }
     }
 
     public static void main(String[] args) {
 
-        System.out.println("Smart Resume Builder Backend Started Successfully");
+        System.out.println("Smart Resume Builder Backend Started");
+
     }
 }
